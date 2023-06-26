0bb007815c88c0d559b33df66ead81245bef5921
parthvasoya59 <parthvasoya1000@gmail.com>
2023-06-25 21:59:01 -0400

    Profile Screen Started

--- lib/Screen/profile_screen/profile_screen_logic.dart
+++ lib/Screen/profile_screen/profile_screen_logic.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gymworkout/Screen/home_screen/home_screen_logic.dart';
 
 import '../../AppRoute/app_route.dart';
 import '../../Constant/app_constant.dart';
@@ -10,10 +11,49 @@ import '../../Utils/preference.dart';
 
 class ProfileScreenLogic extends GetxController {
 
+  var homeCon = Get.find<HomeScreenLogic>();
+
+  RxDouble bmiValue = 0.00.obs, weightValue = 45.0.obs, heightValue = 130.0.obs;
+  RxString keyForMap = "".obs, goal = "".obs;
+
+  loadData() async {
+    weightValue.value = double.parse(await Preference().read(Const.prefWeight));
+    heightValue.value = double.parse(await Preference().read(Const.prefHeight));
+    debugPrint('onNextBmi => weight ${weightValue.value} kg');
+    debugPrint('onNextBmi => height ${heightValue.value} cm');
+    bmiValue.value = (weightValue.value / ((heightValue.value/100) * (heightValue.value/100))).toPrecision(2);
+    debugPrint('onNextBmi => bmiValue.value ${bmiValue.value}');
+
+    if(bmiValue.value <= 18.5){
+      keyForMap.value = "Underweight";
+    } else if(bmiValue.value > 18.5 && bmiValue.value <= 24.9){
+      keyForMap.value = "Normal weight";
+    } else if(bmiValue.value > 24.9 && bmiValue.value <= 29.9){
+      keyForMap.value = "Overweight";
+    } else{
+      keyForMap.value = "Obesity";
+    }
+
+    int goalIndex = await Preference().readInt(Const.prefGoalIndex);
+    debugPrint('getInitiData => goalIndex 1 : ${goalIndex}');
+    //0 = Muscle Gain, 1 = Weight Loss, 2 = Fitness, 3 = Wellness
+    if(goalIndex == 0){
+      goal.value = "Muscle Gain";
+    }else if(goalIndex == 1){
+      goal.value = "Weight Loss";
+    }else if(goalIndex == 2){
+      goal.value = "Fitness";
+    }else{
+      goal.value = "Wellness";
+    }
+
+  }
+
   @override
   void onInit() async {
     // TODO: implement onInit
     super.onInit();
+    loadData();
   }
 
   @override
--- lib/Screen/profile_screen/profile_screen_view.dart
+++ lib/Screen/profile_screen/profile_screen_view.dart
@@ -1,4 +1,5 @@
 import 'package:gymworkout/Constant/colors.dart';
+import '../../Constant/app_image.dart';
 import '../../Constant/appstyle.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -23,25 +24,106 @@ class ProfileScreenWidget extends GetView<ProfileScreenLogic> {
       backgroundColor: cBlack,
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(w * 0.05),
+          padding: EdgeInsets.all(w * 0.10),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              SizedBox(height: h * 0.05,),
               Column(
                 children: [
-                  Text(
-                    "Profile Screen!",
-                    textAlign: TextAlign.center,
-                    style: AppStyle.textStyleLatoBoldWhite30,
+                  Container(
+                    width: w * 0.35,
+                    height: w * 0.35,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              Assets.hometoplogo,
+                            ),
+                            filterQuality: FilterQuality.high,
+                            fit: BoxFit.contain
+                        )
+                    ),
                   ),
                 ],
               ),
+              SizedBox(height: h * 0.05,),
+              Column(
+                children: [
+                  Obx(() {
+                    return profileTab(
+                        "Gender ", controller.homeCon.isMale.value == true
+                        ? "Male"
+                        : "Female", w);
+                  }),
+                  customeSizedBox(h),
+                  Obx(() {
+                    return profileTab(
+                        "Height ", controller.heightValue.value.round().toString() + " cm = " +
+                        (controller.heightValue.value / 30.48).toPrecision(2).toString() + " ft", w);
+                  }),
+                  customeSizedBox(h),
+                  Obx(() {
+                    return profileTab(
+                        "Weight ", controller.weightValue.value.toString() + " kg = " +
+                        (controller.weightValue.value * 2.20462262185).toPrecision(2).toString() + " lb", w);
+                  }),
+                  customeSizedBox(h),
+                  Obx(() {
+                    return profileTab(
+                        "BMI  ", controller.bmiValue.value.toString(), w);
+                  }),
+                  customeSizedBox(h),
+                  Obx(() {
+                    return profileTab(
+                        "Category  ", controller.keyForMap.value.toString(), w);
+                  }),
+                  customeSizedBox(h),
+                  Obx(() {
+                    return profileTab(
+                        "Goal  ", controller.goal.value.toString(), w);
+                  }),
+                  customeSizedBox(h),
+                  customeSizedBox(h),
+                ],
+              )
             ],
           ),
         ),
       ),
     );
   }
+
+  SizedBox customeSizedBox(double h){
+    return SizedBox(height: h * 0.02,);
+  }
+
+  Row profileTab(String text1, String text2, w) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 2,
+          child: Text(
+              text1,
+              style: AppStyle.textStyleLatoMediumWhite20
+          ),
+        ),
+        Text(
+            ":",
+            style: AppStyle.textStyleLatoMediumWhite20
+        ),
+        SizedBox(width: w * 0.01,),
+        Expanded(
+          flex: 4,
+          child: Text(
+              text2,
+              style: AppStyle.textStyleLatoMediumWhite20
+          ),
+        ),
+      ],
+    );
+  }
+
 }
 
